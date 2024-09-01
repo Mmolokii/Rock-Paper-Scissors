32edd9be30489227a7d6d410254379d21c4f3fcb
Mmolokii <mmoloki.kgolo@gmail.com>
2024-09-01 20:54:28 +0200

    game logic

--- index.js
+++ index.js
@@ -25,5 +25,55 @@ function getHumanChoice(){
   }
 }
 
-console.log(getComputerChoice());
-console.log(getHumanChoice()); 
+// const humanSelection = getHumanChoice(); 
+// const computerSelection = getComputerChoice(); 
+
+function checkWinner(humanSelection, computerSelection){
+  if(humanSelection ==  computerSelection){
+    return "Tie"; 
+  } else if((humanSelection == "rock" && computerSelection == "scissors") || 
+  (humanSelection== "paper" && computerSelection == "rock") ||
+  (humanSelection == "scissors" && computerSelection == "paper")){
+    return "Player";
+  } else {
+    return "Computer";
+  }
+}
+
+// function playRound(humanSelection, computerSelection){
+//   const result = checkWinner(humanSelection, computerSelection);
+
+//   if(result == "tie"){
+//     return "It's a Tie!";
+//   } else if(result == "Player"){
+//     return `You Win! ${humanSelection} beats ${computerSelection}`;
+//   } else {
+//     return `You Lost! ${computerSelection} beats ${humanSelection}`;
+//   }
+// }
+
+function game(){
+  let humanScore = 0; 
+  let computerScore = 0;
+
+  for(let i = 0; i < 5; i++){
+    const playerSelection = getHumanChoice(); 
+    const computerSelection = getComputerChoice(); 
+
+    if(checkWinner(playerSelection, computerSelection) == "Player"){
+      humanScore++; 
+    } else if(checkWinner(playerSelection, computerSelection) == "computer"){
+      computerScore++; 
+    }
+  }
+
+  if(humanScore > computerScore){
+    console.log("You Won The Game!"); 
+  } else if(humanScore < computerScore){
+    console.log("You Lost The Game!"); 
+  } else {
+    console.log("The Game was Tied!"); 
+  }
+}
+
+game(); 
