2a2fb758e7821e93ae349df0326b27b11723826a
Mmolokii <mmoloki.kgolo@gmail.com>
2024-09-01 12:46:51 +0200

    Getting user input

--- index.js
+++ index.js
@@ -7,4 +7,23 @@ function getComputerChoice(){
   return choice; 
 }
 
+function getHumanChoice(){
+  let validInput = false; 
+
+  while(validInput == false){
+    const userChoice = prompt("Select Rock, Paper or Scissors!");
+    
+    if(userChoice == null){
+      continue; 
+    }
+
+    const userChoiceLower = userChoice.toLowerCase(); 
+    if(options.includes(userChoiceLower) == true){
+      validInput = true; 
+      return userChoiceLower; 
+    }
+  }
+}
+
 console.log(getComputerChoice());
+console.log(getHumanChoice()); 
