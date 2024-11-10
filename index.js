ae9b2efb806dd01edb707b98fd4de299027af338
Mmolokii <mmoloki.kgolo@gmail.com>
2024-11-10 15:31:29 +0200

    Adding an updateScore function

--- index.js
+++ index.js
@@ -2,27 +2,23 @@ let playerScore = 0;
 let computerScore = 0; 
 let roundWinner = '';
 
-function playRound(playerChoice, computerChoice){
-  if(playerChoice === computerChoice){
-    roundWinner = 'tie'
-  }
+function playRound(playerChoice){
 
-  if(
+    const computerChoice = getComputerChoice();
+    let result = ''; 
+
+  if(playerChoice === computerChoice){
+    result = "It's a tie!"; 
+  } else if (
     (playerChoice === 'ROCK' && computerChoice === 'SCISSORS') ||
     (playerChoice === 'PAPER' && computerChoice === 'ROCK') ||
     (playerChoice === 'SCISSORS' && computerChoice === 'PAPER') 
   ){
     playerScore++
-    roundWinner = 'player'
-  }
-
-  if(
-    (computerChoice === 'ROCK' && playerChoice === 'SCISSORS') ||
-    (computerChoice === 'PAPER' && playerChoice === 'ROCK') ||
-    (computerChoice === 'SCISSORS' && playerChoice === 'PAPER') 
-  ){
+    result = `You win! ${playerSelection} beats ${computerSelection}`; 
+  } else {
     computerChoice++
-    roundWinner = 'computer'
+    result = `You lose! ${computerSelection} beats ${playerSelection}`;
   }
   // add an update score section
 }
@@ -31,10 +27,24 @@ function getComputerChoice(){
   let randomNumber = Math.floor((Math.random() * 3) +1) 
   switch(randomNumber){
     case 1: 
-      return 'ROCK'
+      return 'ROCK';
     case 2: 
-      return 'PAPER'
+      return 'PAPER';
     case 3: 
-      return 'SCISSORS '
+      return 'SCISSORS'; 
   }
 }
+
+function updateScore(){
+  document.getElementById("score").textConent = `Player: ${playerScore} - Computer: ${computerScore}`;
+}
+
+function isGameOver(){
+  return playerScore === 5 || playerScore === 5
+}
+
+
+
+const btnRock = document.querySelector('#rockBtn'); 
+const btnPaper = document.querySelector('#paperBtn')
+const btnScissors = document.querySelector('#scissorsBtn')
